05ba89a0fb5b6109048977e90abe41765d06fee5
ander <andereiros@gmail.com>
2022-03-20 16:24:22 +0100

    Creada la tabla Pokemon y metidos 3 pokemon de prueba

--- app/src/main/java/com/example/individual1/MiBD.java
+++ app/src/main/java/com/example/individual1/MiBD.java
@@ -17,8 +17,24 @@ public class MiBD extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase){
 
         String query = "CREATE TABLE Usuarios(usuario VARCHAR(50) PRIMARY KEY NOT NULL, contraseña VARCHAR(50))";
-        Log.d("Pruebita", query);
+        Log.d("Creada tabla Usuarios", query);
         sqLiteDatabase.execSQL(query);
+
+        String query2 = "CREATE TABLE Pokemon(nombre VARCHAR(50) PRIMARY KEY NOT NULL, tipo1 VARCHAR(50), tipo2 VARCHAR(50))";
+        Log.d("Creada tabla pokemon", query2);
+        sqLiteDatabase.execSQL(query2);
+
+        String pokemon1 = "INSERT INTO Pokemon Values('Charmander','Fuego','Ninguno')";
+        Log.i("Insertar pokemon1", pokemon1);
+        sqLiteDatabase.execSQL(pokemon1);
+
+        String pokemon2 = "INSERT INTO Pokemon Values('Squirtle','Agua','Ninguno')";
+        Log.i("Insertar pokemon1", pokemon2);
+        sqLiteDatabase.execSQL(pokemon2);
+
+        String pokemon3 = "INSERT INTO Pokemon Values('Bulbasaur','Planta','Veneno')";
+        Log.i("Insertar pokemon1", pokemon3);
+        sqLiteDatabase.execSQL(pokemon3);
 
     }
 
